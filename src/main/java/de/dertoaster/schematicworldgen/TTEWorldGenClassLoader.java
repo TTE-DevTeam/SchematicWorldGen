c4ecd8989e758614e11f412bc2ad8d764f8e7bc1
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2026-05-15 20:57:24 +0200

    remove WE API from classloader; Depend on the plugin

--- src/main/java/de/dertoaster/schematicworldgen/TTEWorldGenClassLoader.java
+++ src/main/java/de/dertoaster/schematicworldgen/TTEWorldGenClassLoader.java
@@ -11,14 +11,8 @@ public final class TTEWorldGenClassLoader implements PluginLoader {
 
     @Override
     public void classloader(PluginClasspathBuilder classpathBuilder) {
-        // WorldEdit API
-        MavenLibraryResolver resolver = new MavenLibraryResolver();
-        resolver.addRepository(new RemoteRepository.Builder("Enginehub", "default", "https://maven.enginehub.org/repo/").build());
-        resolver.addDependency(new Dependency(new DefaultArtifact("com.sk89q.worldedit:worldedit-core:7.3.0"), null));
-        classpathBuilder.addLibrary(resolver);
-
         // FastUtil
-        resolver = new MavenLibraryResolver();
+        MavenLibraryResolver resolver = new MavenLibraryResolver();
         resolver.addRepository(new RemoteRepository.Builder("mvnrepository", "default", MavenLibraryResolver.MAVEN_CENTRAL_DEFAULT_MIRROR).build());
         resolver.addDependency(new Dependency(new DefaultArtifact("it.unimi.dsi:fastutil:8.5.11"), null));
         classpathBuilder.addLibrary(resolver);

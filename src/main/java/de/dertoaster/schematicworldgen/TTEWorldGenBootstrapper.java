9e27d6e1ac3307476e2f771d51fec4380d7277df
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2026-05-15 18:51:03 +0200

    registration must happen in bootstrap, otherwise registries will be frozen

--- src/main/java/de/dertoaster/schematicworldgen/TTEWorldGenBootstrapper.java
+++ src/main/java/de/dertoaster/schematicworldgen/TTEWorldGenBootstrapper.java
@@ -1,12 +1,21 @@
 package de.dertoaster.schematicworldgen;
 
+import de.dertoaster.schematicworldgen.feature.registry.SchematicFeatures;
 import io.papermc.paper.plugin.bootstrap.BootstrapContext;
 import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.Identifier;
 
 public final class TTEWorldGenBootstrapper implements PluginBootstrap {
 
     @Override
     public void bootstrap(BootstrapContext context) {
+        Registry.register(
+                BuiltInRegistries.FEATURE,
+                Identifier.fromNamespaceAndPath("schematicworldgen", "schematic"),
+                SchematicFeatures.SCHEMATIC_FEATURE.get()
+        );
     }
 
 }
--- src/main/java/de/dertoaster/schematicworldgen/TTEWorldGenPlugin.java
+++ src/main/java/de/dertoaster/schematicworldgen/TTEWorldGenPlugin.java
@@ -1,13 +1,6 @@
 package de.dertoaster.schematicworldgen;
 
-import de.dertoaster.schematicworldgen.feature.ResourceReloadHandler;
-import de.dertoaster.schematicworldgen.feature.registry.SchematicFeatures;
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.Identifier;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.packs.resources.ReloadableResourceManager;
-import org.bukkit.craftbukkit.CraftServer;
+import de.dertoaster.schematicworldgen.feature.SchematicFeatureBootstrap;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TTEWorldGenPlugin extends JavaPlugin {
@@ -16,15 +9,7 @@ public class TTEWorldGenPlugin extends JavaPlugin {
     public void onLoad() {
         super.onLoad();
 
-        MinecraftServer server = ((CraftServer) this.getServer()).getServer();
-        ReloadableResourceManager reloadableResourceManager = (ReloadableResourceManager) server.getResourceManager();
-        reloadableResourceManager.registerReloadListener(new ResourceReloadHandler());
-
-        Registry.register(
-                BuiltInRegistries.FEATURE,
-                Identifier.fromNamespaceAndPath("schematicworldgen", "schematic"),
-                SchematicFeatures.SCHEMATIC_FEATURE.get()
-        );
+        SchematicFeatureBootstrap.init();
     }
 
 }
